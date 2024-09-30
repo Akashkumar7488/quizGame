06045f8034e249472fc9812f926f9a33bdd34cdb
Akashkumar7488 <138307042+Akashkumar7488@users.noreply.github.com>
2024-09-30 16:35:31 +0530

    update my project

--- src/modal/userModal.ts
+++ src/modal/userModal.ts
@@ -1,76 +1,44 @@
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema({
-//     username:{
-//         type:String,
-//         required:true
-//     },
-//     email:{
-//         type:String,
-//         required:true
-//     },
-//     password:{
-//         type:String,
-//         required:true
-//     },
-// },{timestamps:true})
-
-// const User = mongoose.models.users || mongoose.model("users",userSchema);
-// export default User;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, "Please provide a username"],
-        unique: true,
+    username:{
+        type:String,
+        required:true
     },
-    email: {
-        type: String,
-        required: [true, "Please provide an email"],
-        unique: true,
+    email:{
+        type:String,
+        required:true
     },
-    password: {
-        type: String,
-        required: [true, "Please provide a password"],
+    password:{
+        type:String,
+        required:true
     },
-    isVerified: {
-        type: Boolean,
-        default: false,
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false,
-    },
-    forgotPasswordToken: String,
-    forgotPasswordTokenExpiry: Date,
-    verifyToken: String,
-    verifyTokenExpiry: Date,
-})
+},{timestamps:true})
 
-export const User = mongoose.models.users || mongoose.model("users", userSchema);
+const User = mongoose.models.users || mongoose.model("users",userSchema);
+export default User;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
