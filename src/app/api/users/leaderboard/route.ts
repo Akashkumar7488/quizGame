46e9c189f3f99154e8f0a1efd6110397bd0e30f1
Akashkumar7488 <138307042+Akashkumar7488@users.noreply.github.com>
2024-10-03 18:14:53 +0530

    update1 commit

--- src/app/api/users/leaderboard/route.ts
+++ src/app/api/users/leaderboard/route.ts
@@ -23,34 +23,7 @@
 
 
 
-<<<<<<< HEAD
 // app/api/users/leaderboard.js
-=======
-// pages/api/users/leaderboard.js
-// import { NextRequest, NextResponse } from 'next/server';
-// import { connect } from '@/database/dbConnection';
-// import Score from '@/modal/scoreModal'; // Import your score model
-// import User from '@/modal/userModal'; // Import your user model
-
-// export async function GET(request: NextRequest) {
-//   try {
-//     await connect();
-
-//     // Fetch the leaderboard with populated username
-//     const leaderboard = await Score.find()
-//       .populate('userId', 'username') // Fetch 'username' from User model
-//       .sort({ score: -1 }) // Sort by score in descending order
-//       // .limit(10); // Limit to top 10 scores
-
-//     return NextResponse.json(leaderboard);
-//   } catch (error) {
-//     console.error('Error fetching leaderboard:', error);
-//     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-//   }
-// }
-
-// pages/api/users/leaderboard.js
->>>>>>> aa22193baa9c9363a47958bca7f9315f83675a0a
 import { NextRequest, NextResponse } from 'next/server';
 import { connect } from '@/database/dbConnection';
 import Score from '@/modal/scoreModal'; // Import your score model
